8df8117d19fca6c6ea9d667ae6352ca9312e1eeb
echanovevaro <echanovevaro@gmail.com>
2024-01-17 19:04:21 +0100

    fix 7 loader en gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -85,7 +85,7 @@ export default function Gallery({ coleccion }) {
                 <>
                   {!loaded && (
                     <div
-                      class="absolute top-1/3 -z-1 left-1/2 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-neutral-300 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                      class="absolute inset-auto h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-neutral-300 motion-reduce:animate-[spin_1.5s_linear_infinite]"
                       role="status"
                     >
                       <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
