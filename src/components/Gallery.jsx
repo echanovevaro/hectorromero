80d38d1b13ed8ded82b133363b795c6cdaac2be9
echanovevaro <echanovevaro@gmail.com>
2024-01-18 15:20:41 +0100

    gallery fix 2 fullscreen

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -141,30 +141,7 @@ export default function Gallery({ coleccion }) {
                       handleExitFullScreenClick();
                       setFullPage(false);
                     }}
-                  >
-                    <button
-                      className="absolute top-0 right-0 p-4"
-                      onClick={() => {
-                        handleExitFullScreenClick();
-                        setFullPage(false);
-                      }}
-                    >
-                      <svg
-                        className="h-6 w-6 text-neutral-400 cursor-pointer"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </div>
+                  />
                 </>
               )}
             </div>
