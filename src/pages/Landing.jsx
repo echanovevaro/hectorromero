0eaec956672aae1a9534dee347c3fa2b09ec3adf
echanovevaro <echanovevaro@gmail.com>
2024-01-24 19:24:31 +0100

    animacion muestra fix 6

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -9,7 +9,7 @@ function Landing() {
     speed: -200,
   });
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, amount: 0.8 });
+  const isInView = useInView(ref, { once: true, amount: 0.9 });
 
   useEffect(() => {
     const timeout = setTimeout(() => {
