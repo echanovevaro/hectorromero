1646decb3e53199279c63e2ff97c62fa0b84511d
echanovevaro <30557018+echanovevaro@users.noreply.github.com>
2024-01-24 11:44:19 +0100

    destacados font size

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -87,7 +87,7 @@ function Landing() {
               <img src="/Entorno bloqueador 50x50cm-compressed.jpg" />
             </div>
             <div className="content1Title">
-              <span className="border-t border-neutral-300 text-[0.5rem]">
+              <span className="border-t border-neutral-300">
                 Entorno bloqueador
               </span>
             </div>
@@ -95,25 +95,19 @@ function Landing() {
               <img src="/tensión 50x50 cm-compressed.jpg" />
             </div>
             <div className="content2Title">
-              <span className="border-t border-neutral-300 text-[0.5rem]">
-                Tensión
-              </span>
+              <span className="border-t border-neutral-300">Tensión</span>
             </div>
             <div className="content3">
               <img src="/Futuro bloque 120x194cm-compressed.jpg" />
             </div>
             <div className="content3Title">
-              <span className="border-t border-neutral-300 text-[0.5rem]">
-                Futuro bloque
-              </span>
+              <span className="border-t border-neutral-300">Futuro bloque</span>
             </div>
             <div className="content4">
               <img src="/Blo questions 39x43 cm-compressed.jpg" />
             </div>
             <div className="content4Title">
-              <span className="border-t border-neutral-300 text-[0.5rem]">
-                Blo questions
-              </span>
+              <span className="border-t border-neutral-300">Blo questions</span>
             </div>
             <div className="content5">
               <img src="/20200608_202759.gif" />
@@ -122,9 +116,7 @@ function Landing() {
               <img src="/20200608_202759.gif" />
             </div>
             <div className="content5-6Title">
-              <span className="border-t border-neutral-300 text-[0.5rem]">
-                Te bloqueo
-              </span>
+              <span className="border-t border-neutral-300">Te bloqueo</span>
             </div>
           </div>
         </div>
