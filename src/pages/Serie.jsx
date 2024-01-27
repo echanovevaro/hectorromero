231b7c011eeacd44da4fb7e5012ee6257c8152ec
echanovevaro <echanovevaro@gmail.com>
2024-01-27 05:34:05 +0100

    fix overflow footer

--- src/pages/Serie.jsx
+++ src/pages/Serie.jsx
@@ -18,7 +18,7 @@ export default function Serie() {
       <ScrollToTop />
       <div className="overflow-y-scroll overflow-x-hidden">
         <MainNavigation />
-        <div className="min-h-[103dvh]">
+        <div className="min-h-[103vh]">
           <Gallery coleccion={data} />
         </div>
         <Footer />
