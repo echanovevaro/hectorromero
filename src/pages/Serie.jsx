25ddb02cfb72b2b36cf11203e386844e8f273190
echanovevaro <echanovevaro@gmail.com>
2024-01-27 03:28:06 +0100

    fix revert

--- src/pages/Serie.jsx
+++ src/pages/Serie.jsx
@@ -18,7 +18,7 @@ export default function Serie() {
       <ScrollToTop />
       <div className="overflow-y-scroll">
         <MainNavigation />
-        <div className="min-h-[100dvh]">
+        <div className="min-h-[95dvh]">
           <Gallery coleccion={data} />
         </div>
         <Footer />
