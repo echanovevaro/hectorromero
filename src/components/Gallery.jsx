582192ee4a59f5f1c7615dc5d2cda0fcfadaebc7
echanovevaro <echanovevaro@gmail.com>
2024-01-30 19:48:25 +0100

    tapaar titulo en fullpage gallery 4

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -112,7 +112,7 @@ export default function Gallery({ coleccion }) {
                 transition={{ duration: 0.8, type: "spring" }}
                 onPanStart={onDetallePanStart}
                 onPanEnd={onDetallePanEnd}
-                className="bg-white absolute w-screen z-[150] h-screen inset-0 touch-pinch-zoom"
+                className="bg-white absolute w-screen z-[600] h-screen inset-0 touch-pinch-zoom"
               >
                 <motion.div
                   variants={{
@@ -126,7 +126,7 @@ export default function Gallery({ coleccion }) {
                   onPanEnd={onDetallePanEnd}
                   transition={{ duration: 0.8, type: "spring" }}
                   id="fullPage"
-                  className="w-full h-full bg-contain bg-no-repeat bg-center bg-white z-[200] landscape:-top-[3.5rem] landscape:bottom-[3.5rem] touch-pinch-zoom"
+                  className="w-full h-full bg-contain bg-no-repeat bg-center bg-white z-[650] landscape:-top-[3.5rem] landscape:bottom-[3.5rem] touch-pinch-zoom"
                   style={{
                     backgroundImage: `url(${obra.imagenURL})`,
                   }}
