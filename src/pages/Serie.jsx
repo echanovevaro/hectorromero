78a8c6d705f7c5e4274f9cc89cc3cc261c9b4000
echanovevaro <echanovevaro@gmail.com>
2024-01-30 11:23:32 +0100

    deshacer prueba gallery landscape y reducir espacio superior galeria

--- src/pages/Serie.jsx
+++ src/pages/Serie.jsx
@@ -18,7 +18,7 @@ export default function Serie() {
       <ScrollToTop />
       <div className="overflow-y-scroll overflow-x-hidden">
         <MainNavigation />
-        <div className="h-screen relative">
+        <div className="h-screen landscape:h-[155vh] relative">
           <Gallery coleccion={data} />
         </div>
         <Footer />
