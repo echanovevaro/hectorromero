ca0f1db3e10c3fcae555cb7ddd0416b77a6b040e
echanovevaro <echanovevaro@gmail.com>
2024-01-31 18:08:53 +0100

    fix padding premios

--- src/pages/PremiosPage.jsx
+++ src/pages/PremiosPage.jsx
@@ -1,17 +1,17 @@
-import Footer from "../components/Footer"
-import MainNavigation from "../components/MainNavigation"
-import Premios from "../components/Premios"
+import Footer from "../components/Footer";
+import MainNavigation from "../components/MainNavigation";
+import Premios from "../components/Premios";
 
 const PremiosPage = () => {
   return (
     <>
       <MainNavigation />
-      <div className="pb-32 pt-[6rem] text-xs px-[1rem] overflow-hidden w-screen">
+      <div className="pb-32 pt-[6rem] text-xs overflow-hidden w-screen">
         <Premios />
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default PremiosPage
+export default PremiosPage;
