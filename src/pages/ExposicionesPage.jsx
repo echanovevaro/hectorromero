6bf80dc0955efee5d474ba21681097282372ff20
echanovevaro <echanovevaro@gmail.com>
2024-02-24 19:06:38 +0100

    quitar fix temporal expos

--- src/pages/ExposicionesPage.jsx
+++ src/pages/ExposicionesPage.jsx
@@ -1,51 +1,47 @@
-import { ScrollRestoration } from "react-router-dom"
-import Footer from "../components/Footer"
-import MainNavigation from "../components/MainNavigation"
-import Exposiciones from "../components/Exposiciones"
-import { fetchAll } from "../http"
-import { useQuery } from "@tanstack/react-query"
+import { ScrollRestoration } from "react-router-dom";
+import Footer from "../components/Footer";
+import MainNavigation from "../components/MainNavigation";
+import Exposiciones from "../components/Exposiciones";
+import { fetchAll } from "../http";
+import { useQuery } from "@tanstack/react-query";
 
 const ExposicionesPage = () => {
   const { data } = useQuery({
     queryKey: ["exposiciones"],
     queryFn: () => fetchAll("exposiciones"),
-  })
+  });
 
   const exposicionesFinalizadas = data
     ?.filter((exposicion) => {
-      const fecha = new Date(exposicion.fecha)
-      const hoy = new Date()
-      return fecha < hoy
+      const fecha = new Date(exposicion.fecha);
+      const hoy = new Date();
+      return fecha < hoy;
     })
-    .sort((a, b) => new Date(b.fecha) - new Date(a.fecha))
+    .sort((a, b) => new Date(b.fecha) - new Date(a.fecha));
 
   const exposicionesProximas = data
     ?.filter((exposicion) => {
-      const fecha = new Date(exposicion.fecha)
-      const hoy = new Date()
-      return fecha >= hoy
+      const fecha = new Date(exposicion.fecha);
+      const hoy = new Date();
+      return fecha >= hoy;
     })
-    .sort((a, b) => new Date(b.fecha) - new Date(a.fecha))
+    .sort((a, b) => new Date(b.fecha) - new Date(a.fecha));
 
   return (
     <>
       <ScrollRestoration />
       <MainNavigation />
       <div className="pb-16 pt-[6rem] text-xs overflow-hidden w-full">
-        {/* {data && (
+        {data && (
           <Exposiciones
             finalizadas={exposicionesFinalizadas}
             proximas={exposicionesProximas}
           />
-        )} */}
-        <Exposiciones
-          finalizadas={exposicionesFinalizadas}
-          proximas={exposicionesProximas}
-        />
+        )}
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default ExposicionesPage
+export default ExposicionesPage;
