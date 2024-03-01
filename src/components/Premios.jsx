936967117b976acb9d0fd84f04d891efe6cebcb5
echanovevaro <echanovevaro@gmail.com>
2024-03-01 23:33:50 +0100

    fix

--- src/components/Premios.jsx
+++ src/components/Premios.jsx
@@ -174,7 +174,7 @@ const Premios = ({ data }) => {
                     viewBox="0 0 24 24"
                     strokeWidth={1}
                     stroke="currentColor"
-                    className="w-3 h-3 lg:w-4 lg:h-4"
+                    className="w-3 h-3"
                   >
                     <path
                       strokeLinecap="round"
