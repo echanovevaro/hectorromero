304c1137498defa822253937db78dbcb65174fcd
echanovevaro <echanovevaro@gmail.com>
2024-03-02 09:31:15 +0100

    subir obra en seccion

--- src/pages/Obra.jsx
+++ src/pages/Obra.jsx
@@ -1,25 +1,25 @@
-import MainNavigation from "../components/MainNavigation"
-import Footer from "../components/Footer"
-import ObraMenu from "../components/ObraMenu"
-import { ScrollRestoration } from "react-router-dom"
-import { useQuery } from "@tanstack/react-query"
-import { fetchAll } from "../http"
+import MainNavigation from "../components/MainNavigation";
+import Footer from "../components/Footer";
+import ObraMenu from "../components/ObraMenu";
+import { ScrollRestoration } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAll } from "../http";
 
 function Obra() {
   const { data } = useQuery({
     queryKey: ["obras"],
     queryFn: () => fetchAll("obras"),
-  })
+  });
 
   return (
     <>
       <ScrollRestoration />
       <MainNavigation />
-      <div className="bg-white w-full z-10 pt-[6rem] pb-8 landscape:h-full min-h-[70dvh]">
+      <div className="bg-white w-full pb-8 mt-[4rem]">
         {data && <ObraMenu data={data} />}
       </div>
       <Footer />
     </>
-  )
+  );
 }
-export default Obra
+export default Obra;
