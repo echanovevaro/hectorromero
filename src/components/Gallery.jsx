cd178039da43f98ac0b35c5273e5fb1660b7fe25
echanovevaro <30557018+echanovevaro@users.noreply.github.com>
2024-03-04 17:20:40 +0100

    restaurar gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -132,7 +132,7 @@ export default function Gallery({ coleccion }) {
             )
         })}
       </AnimatePresence>
-      <div className="flex flex-col justify-start items-start gap-2 pt-[5rem] max-w-[1344px] min[1600px]:mx-auto mx-[1rem] lg:mx-[8rem]">
+      <div className="flex flex-row justify-start items-end gap-2 pt-[5rem] max-w-[1344px] min[1600px]:mx-auto mx-[1rem] lg:mx-[8rem]">
         <h1 className="uppercase text-base lg:text-xl  opacity-[0.7]">
           {serie}
         </h1>
